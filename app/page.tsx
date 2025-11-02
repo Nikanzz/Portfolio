31b60ca6fae57b0c725ae40a22f8a13cf1c6afd1
Jeremia <jeremiap787@gmail.com>
2025-11-02 14:30:33 +0700

    feat: update Home component to include Projects and Footer sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Footer from "@/components/main/Footer";
 import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
       <div className="flex flex-col gap-20 h-[850px]">
         <Hero />
         <Skills />
+        <Projects />
+        <Footer />
       </div>
     </main>
   );
